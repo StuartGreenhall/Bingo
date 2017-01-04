fe148231eab0b76e78a0013eef7072c35436d454
Stuart Greenhall <stuart_greenhall@hotmail.com>
2017-01-04 09:39:30 +0000

    Stu : finally happy with initial structure

---
+++ src/Bingo.Web/Services/BingSearchService.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication.Services
+{
+    public class BingSearchService
+    {
+        public bool Search(string SearchTerm)
+        {
+            return true;
+        }
+    }
+}
---
+++ test/Unit.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using WebApplication.Services;
+
+namespace Unit.Tests
+{
+    [TestFixture]
+    public class BingSearchServiceTests
+    {
+        [Test]
+        public void ItShouldWork() 
+        {
+            var bing = new BingSearchService();
+            Assert.True(bing.Search("wibble")); 
+        }
+    }
+}

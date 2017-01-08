1b91dcb0ded328cf6ccafe20e75588869f69112a
Stuart Greenhall <stuart_greenhall@hotmail.com>
2017-01-08 19:34:41 +0000

    Stu: wiring up download button.

--- src/Bingo.Web/wwwroot/js/site.js
+++ src/Bingo.Web/wwwroot/js/site.js
@@ -1,1 +1,9 @@
 // Write your Javascript code.
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+
+$('#download-btn').on('click', function(evt) {
+    console.log('clicked');
+});
